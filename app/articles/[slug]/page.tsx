6dc37c75736f5d14a6a545384f69fca3d9d0c686
Avebela <ninabesolova@gmail.com>
2024-03-05 12:40:07 +0100

    prjamo

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -11,7 +11,7 @@ export const revalidate = 30;
 
 const getOneArticle = async (slug: any) => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/articles?filters[slug]=${slug}`
+    `http://127.0.0.1:1337/api/articles?filters[slug]=${slug}`
   );
   const data = await res.json();
 
